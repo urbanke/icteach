a6c24e25815776c0f00e40b3d5a6ce95a5bc21fc
Ruediger Urbanke <ruediger.urbanke@epfl.ch>
2012-05-12 17:44:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icteach::Application.config.secret_token = 'faadced3b6d5971b95b3a6cec965127b9bf39fc80cf26646599a2cdfc123b58a64994aa7e45e024d4c9383fbe0069df0b0969e12786b3c903181ba371cce05e3'
